0deae8764e6b120aec8292adcb2c7583dd5b1752
VirtualOilCake <34388992+VirtualOilCake@users.noreply.github.com>
2022-03-28 17:02:50 -0500

    something

---
+++ Better.js
@@ -0,0 +1,34 @@
+/**
+ * A representation of a curve on canvas
+ */
+class Curve{
+
+}
+
+/**
+ * A factory that produces Curve object
+ */
+class CurveFactory{
+
+}
+
+/**
+ * A manager of CurveFactory
+ */
+class CurveFactoryManager{
+
+}
+
+/**
+ * A factory that produces CurveFactoryManager object
+ */
+class CurveFactoryManagerFactory{
+
+}
+
+/**
+ * A manager of CurveFactoryManagerFactory
+ */
+class CurveFactoryManagerFactoryManager{
+
+}
--- canvas.js
+++ canvas.js
@@ -1,30 +1,22 @@
 // Get Canvas
-
 const canvas = document.querySelector("canvas");
-
 const context2d = canvas.getContext("2d");
+const nameInput = document.getElementById("nameInput");
+const birthInput = document.getElementById("birthdayInput");
 
-
-var seed = 1;
+let seed = 1;
 
 /**
- * Paint the canvas using name and birth day as input.
+ * Paint the canvas using name and birthday as input.
  */
 function paintCanvas() {
-
-    let nameInput = document.getElementById("nameInput");
-    var birthInput = document.getElementById("birthdayInput");
-
-    //console.log(birthInput.valueAsNumber);
-
     if (!nameInput.value || !birthInput.value) {
-        alert("Input is not vaild.");
+        alert("Input is not valid.");
         return;
     }
 
-
-    height = canvas.height;
-    width = canvas.width;
+    let height = canvas.height;
+    let width = canvas.width;
 
     context2d.fillStyle = "black";
     context2d.fillRect(0, 0, width, height);
@@ -56,7 +48,7 @@ function paintCanvas() {
  * @returns {number}
  */
 function getHash(input) {
-    var hash = 0, i, chr;
+    let hash = 0, i, chr;
     if (input.length === 0) return hash;
     for (i = 0; i < input.length; i++) {
         chr = input.charCodeAt(i);
@@ -64,7 +56,7 @@ function getHash(input) {
         hash |= 0; // Convert to 32bit integer
     }
     return hash;
-};
+}
 
 /**
  * Get a random int
@@ -75,8 +67,7 @@ function getRandomInt(max) {
     return Math.floor(random() * max);
 }
 
-
 function random() {
-    var x = Math.sin(seed++) * 10000;
+    let x = Math.sin(seed++) * 10000;
     return x - Math.floor(x);
 }
